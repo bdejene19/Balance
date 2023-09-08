419ef77d21295cf847b1cba298f9eb69bcc1add9
Bemnet Dejene <bdejene19@gmail.com>
2023-09-07 22:57:55 -0400

    made global folder or reusable/modular approach

---
+++ src/Globals/Resources.tsx
@@ -0,0 +1,16 @@
+export enum Text {
+    HOME = 'Home',
+    SERVICES = 'Services',
+    RATES = 'Massage Rates',
+    HOURS = 'Clinic Hours and Location',
+    FAQs = 'FAQS'
+}
+
+export enum Colors {
+    PRIMARY = '#AAC6CF',
+    SECONDARY = '#052419',
+    TERTIARY = ''
+}
+
+export enum FontSizes {
+}
