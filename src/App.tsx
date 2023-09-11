e22c85a9f80d58a0753f86c0e326704df147cef1
Bemnet Dejene <bdejene19@gmail.com>
2023-09-11 14:35:05 -0400

    Fixed styling bug that removed padding on page refresh (accidentally duplicated CSS boilerplate through styled component wrapper)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import styled from 'styled-components'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home';
 function App() {
   return (
     
-      <Main>
         <Router>
           <Routes>
             <Route path='/' element={<Home/>}/>
@@ -14,14 +12,6 @@ function App() {
       
         </Router>
 
-      </Main>
   );
 }
-const Main = styled.main`
-  & * {
-    box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  }
-`;
 export default App;
